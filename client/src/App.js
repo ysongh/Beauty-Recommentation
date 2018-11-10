c827b444a8839fe17ada201b64bcb252a6011314
Song <yousonghou@yahoo.com>
2018-11-10 01:39:49 +0000

    add dropdown and remove sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -8,47 +8,47 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Beauty Recommentation</h1>
-        <div className="row align-items-start">
-          <div className="col-md-2">
-            <div className="inline bg-light">
-              <p>Outfit</p>
-              <p>Outfit</p>
-              <p>Outfit</p>
-              <p>Outfit</p>
-              <p>Outfit</p>
-            </div>
-          </div>
-          <div className="col-md-10">
-            <div class="card">
-              <div className="row">
-                <div className="col-md-6">
-                  <img className="card-img-top" src={lipstickImg} alt="LipStick" />
-                </div>
-                <div className="col-md-6">
-                  <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="/" className="btn btn-primary">Go somewhere</a>
-                  </div>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-md-6">
-                  <img className="card-img-top" src={lipstickImg} alt="LipStick" />
-                </div>
-                <div className="col-md-6">
-                  <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="/" className="btn btn-primary">Go somewhere</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            
+        <div className="dropdown">
+          <a className="btn btn-secondary dropdown-toggle" href="/" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown link
+          </a>
+          <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <a className="dropdown-item" href="/">Action</a>
+            <a className="dropdown-item" href="/">Another action</a>
+            <a className="dropdown-item" href="/">Something else here</a>
           </div>
         </div>
         
+        <hr />
+        <div className="container">
+          
+          <div class="card">
+            <div className="row">
+              <div className="col-md-6">
+                <img className="card-img-top" src={lipstickImg} alt="LipStick" />
+              </div>
+              <div className="col-md-6">
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="/" className="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <img className="card-img-top" src={lipstickImg} alt="LipStick" />
+              </div>
+              <div className="col-md-6">
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="/" className="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
